30375a43404d86327adb484849d64e3142384fd0
MaciejDubowik <maciejxpoland@gmail.com>
2023-01-20 20:48:39 +0100

    fix

--- lab09/lab09/lab09/main.cpp
+++ lab09/lab09/lab09/main.cpp
@@ -1,18 +1,23 @@
 #include <opencv2/aruco.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace std;
 using namespace cv;
 
-int main()
-{
+int main(){
+    float markerLength = 0.024f;
+
+    Mat cameraMatrix = Mat::eye(3, 3, CV_32F);
+    Mat distCoeffs;
+
     VideoCapture cap(0);
 
-    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::DICT_6X6_250);
-    Ptr<aruco::DetectorParameters> params = aruco::DetectorParameters::create();
+    cv::Ptr<aruco::Dictionary> dictionary = cv::makePtr<aruco::Dictionary>(aruco::getPredefinedDictionary(aruco::DICT_6X6_250));
+    cv::Ptr<aruco::DetectorParameters> params = cv::makePtr<aruco::DetectorParameters>();
 
     while (true) {
         Mat frame;
@@ -23,12 +28,6 @@ int main()
         aruco::detectMarkers(frame, dictionary, corners, ids, params);
 
         Point2f marker1, marker2;
-        if(ids.size() == 2){
-            marker1 = corners[0][0];
-            marker2 = corners[1][0];
-            cout << "marker1 coords: " << marker1;
-            cout << " | marker2 coords: " << marker2 << endl;
-        }
         if(ids.size() == 1){
             marker1 = corners[0][0];
             cout << "marker coords:" << marker1 << endl;
@@ -37,14 +36,31 @@ int main()
             cout << "no markers "<< endl;
         }
        
-        double distance = sqrt(pow(marker1.x - marker2.x, 2) + pow(marker1.y - marker2.y, 2));
-        cout << "distance: " << distance<< endl;
-    
+        if(ids.size() >= 2){
+            vector<Vec3d> rvecs, tvecs;
+            aruco::estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs, rvecs, tvecs);
+        
+            Vec3d translation1 = tvecs[0];
+            Vec3d translation2 = tvecs[1];
+
+            double distance = sqrt(pow(translation1[0] - translation2[0], 2) +
+                                   pow(translation1[1] - translation2[1], 2) +
+                                   pow(translation1[2] - translation2[2], 2));
+            
+            distance = distance * 100;
+            cout << "distance: " << distance << "cm" << endl;
+        }
+
         if (ids.size() > 0) {
             aruco::drawDetectedMarkers(frame, corners);
         }
         imshow("Webcam", frame);
+        
+        if (waitKey(1) == 27) {
+              break;
+          }
     }
 
     return 0;
 }
+
